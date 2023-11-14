2668ddb80d7c7db2e321a6472a72825ab4131c63
TheAIWizard <nathanpanoel@hotmail.fr>
2023-11-14 21:22:58 +0100

    numeric value args fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 # Extract the model URL on TheBloke HF website
 MODEL_URL="$1"
 # Extract additional variables
-N_GPU_LAYERS="${2:-35}"
-N_CTXT="${3:-3000}"
-N_BATCH="${4:-$512}"
-MAIN_GPU="${5:-$0}"
-EMBEDDING="${6:-$False}"
+N_GPU_LAYERS=${2:-35}
+N_CTXT=${3:-3000}
+N_BATCH=${4:-$512}
+MAIN_GPU=${5:-$0}
+EMBEDDING=${6:-$False}
 
 # Extract the model name
 #MODEL_NAME=$(echo "$MODEL_URL" | awk -F'/' '{print $NF}')
@@ -23,4 +23,4 @@ else \
     sh init-script.sh $MODEL_NAME $MODEL_URL
 fi
 
-python3 -m llama_cpp.server --model /models/$MODEL_NAME --ngl $N_GPU_LAYERS --n_ctxt $N_CTXT --n_batch $N_BATCH --main_gpu $MAIN_GPU --embedding $EMBEDDING
+python3 -m llama_cpp.server --model /models/$MODEL_NAME --n_gpu_layers $N_GPU_LAYERS --n_ctxt $N_CTXT --n_batch $N_BATCH --main_gpu $MAIN_GPU --embedding $EMBEDDING
