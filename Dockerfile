e67d6a42bf6be8c66ae75bab953d6c17e3007b41
TheAIWizard <nathanpanoel@hotmail.fr>
2023-11-15 15:25:04 +0100

    load model + volume ok

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 # Use the InseeFrLab Python image with pytorch and GPU integration as the base image
 FROM inseefrlab/onyxia-vscode-pytorch:py3.11.4-gpu
 
+# Bind to a port
+EXPOSE 8000
+
 # Set the working directory
 WORKDIR /app
 
@@ -23,16 +26,18 @@ ENV PATH $PATH:$CUDA_HOME/bin
 # Extract the model URL on TheBloke HF website (default URL)
 ARG MODEL_URL=https://huggingface.co/TheBloke/zephyr-7B-beta-GGUF/resolve/main/zephyr-7b-beta.Q8_0.gguf
 # Extract the model name
-ARG MODEL_NAME=$(echo "$MODEL_URL" | awk -F'/' '{print $NF}')
+#ARG MODEL_NAME=$(echo "$MODEL_URL" | awk -F'/' '{print $NF}')
 
 # Set arguments as environment variables
 ENV MODEL_URL=${MODEL_URL}
-ENV MODEL_NAME=${MODEL_NAME}
+#ENV MODEL_NAME=${MODEL_NAME}
 # Set other environment variables to specify the model
 ENV N_GPU_LAYERS N_CTX N_BATCH MAIN_GPU EMBEDDING
 
 # Mount/Load the model in container if the user wants to load his model on local and not url
 ENV LOAD_MODEL=
+# Create /app/volume directory
+RUN mkdir -p ./models
 # Create a named volume and mount it at /app/models
 VOLUME /app/models
 
@@ -50,7 +55,7 @@ WORKDIR /app
 # Copy the entry point script from the local directory to the container
 COPY docker-run.sh /app
 
-# Launch script to execute the commands with arguments
-ENTRYPOINT [ "./docker-run.sh" ]
+# Launch script to execute the commands with optional arguments
+CMD [ "./docker-run.sh" ]
 # Define arguments for our api server with their defaut value
-CMD [MODEL_URL=$MODEL_URL, N_GPU_LAYERS=0, N_CTX=4096, N_BATCH=100, $MAIN_GPU=0, EMBEDDING=False]
+#CMD [MODEL_URL=$MODEL_URL, N_GPU_LAYERS=0, N_CTX=4096, N_BATCH=100, $MAIN_GPU=0, EMBEDDING=False]
--- docker-run.sh
+++ docker-run.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
 
-# If available locally, load the quantized model in your mounted directory
-LOAD_MODEL=$1
-# Extract the model URL on TheBloke HF website
-export MODEL_URL="$2"
-# Extract additional variables
-export N_GPU_LAYERS=$3 #${3:-5}
-export N_CTX=$4 #${4:-3000}
-export N_BATCH=$5 #${5:-512}
-export MAIN_GPU=$6 #${6:-0}
-export EMBEDDING=$7 #${7:-False}
+echo "Default quantized HF model url: $MODEL_URL"
+
+# Set default values for parameters
+export N_GPU_LAYERS=5
+export N_CTX=3333
+export N_BATCH=512
+export MAIN_GPU=0
+export EMBEDDING=False
 
 # Extract the model name
-MODEL_NAME=$(echo $MODEL_URL | awk -F'/' '{print $NF}')
+export MODEL_NAME=$(echo $MODEL_URL | awk -F'/' '{print $NF}')
 
-# If LOAD_MODEL is set, update MODEL_NAME and attach LOAD_MODEL to a volume
+# If available locally, load the quantized model in your mounted directory --> If LOAD_MODEL is set, update MODEL_NAME and attach LOAD_MODEL to a volume
 if [ -n "$LOAD_MODEL" ]; then \
+    cd ./models # volume path
+    pwd
+    ls -f $LOAD_MODEL
     echo "Using custom model: $LOAD_MODEL"; \
     export MODEL_NAME=$LOAD_MODEL; \
     echo $LOAD_MODEL; \
     echo $N_GPU_LAYERS; \
     echo $N_CTX; \
 else \
-    echo "Using default model: $MODEL_NAME"; \
+    echo "Using url of a quantized HF model: $MODEL_NAME"; \
+    ls -d ./models
+    ls
     # Download model file from url
     sh init-script.sh $MODEL_NAME $MODEL_URL
-fi
 
-python3 -m llama_cpp.server --model "/models/$MODEL_NAME" --n_gpu_layers $N_GPU_LAYERS --n_ctx $N_CTX --n_batch $N_BATCH --main_gpu $MAIN_GPU --embedding $EMBEDDING
+fi
+ls
+pwd
+python3 -m llama_cpp.server --model $MODEL_NAME --n_gpu_layers $N_GPU_LAYERS --n_ctx $N_CTX --n_batch $N_BATCH --main_gpu $MAIN_GPU --embedding $EMBEDDING
