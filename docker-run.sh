5775a72b385877b47962f157e03e2a28f45c3739
TheAIWizard <nathanpanoel@hotmail.fr>
2023-11-16 19:45:46 +0100

    added more arguments good but need to manage the variables with None as default values ...

--- docker-run.sh
+++ docker-run.sh
@@ -3,11 +3,31 @@
 echo "Default quantized HF model url: $MODEL_URL"
 
 # Set default values for parameters
-export N_GPU_LAYERS=5
-export N_CTX=3333
+export N_GPU_LAYERS=0
+export N_CTX=3000
 export N_BATCH=512
 export MAIN_GPU=0
 export EMBEDDING=False
+#export TENSOR_SPLIT=None
+export VOCAB_ONLY=False
+export USE_MMAP=True
+export USE_MLOCK=False
+#export SEED=LLAMA_DEFAULT_SEED
+#export N_THREADS=None
+#export N_THREADS_BATCH=None
+#export ROPE_SCALING_TYPE=LLAMA_ROPE_SCALING_UNSPECIFIED
+export ROPE_FREQ_BASE=0.0
+export ROPE_FREQ_SCALE=0.0
+export MUL_MAT_Q=True
+export F16_KV=True
+export LOGITS_ALL=False
+export LAST_N_TOKENS_SIZE=64
+#export LORA_BASE=None
+#export LORA_PATH=None
+export NUMA=False
+export CHAT_FORMAT=llama-2
+#export CHAT_HANDLER=None
+export VERBOSE=True
 
 # Extract the model name
 export MODEL_NAME=$(echo $MODEL_URL | awk -F'/' '{print $NF}')
@@ -26,4 +46,5 @@ else \
 
 fi
 
-python3 -m llama_cpp.server --host 0.0.0.0 --port 8000 --model $MODEL_NAME --n_gpu_layers $N_GPU_LAYERS --n_ctx $N_CTX --n_batch $N_BATCH --main_gpu $MAIN_GPU --embedding $EMBEDDING
+#full param:python3 -m llama_cpp.server --host 0.0.0.0 --port 8000 --model $MODEL_NAME --n_gpu_layers $N_GPU_LAYERS --n_ctx $N_CTX --n_batch $N_BATCH --main_gpu $MAIN_GPU --embedding $EMBEDDING --n_threads $N_THREADS --n_threads_batch $N_THREADS_BATCH --rope_scaling_type $ROPE_SCALING_TYPE --rope_freq_base $ROPE_FREQ_BASE --rope_freq_scale $ROPE_FREQ_SCALE --mul_mat_q $MUL_MAT_Q --f16_kv $F16_KV --logits_all $LOGITS_ALL --last_n_tokens_size $LAST_N_TOKENS_SIZE --lora_base $LORA_BASE --lora_path $LORA_PATH --numa $NUMA --chat-format $CHAT_FORMAT --chat_handler $CHAT_HANDLER --verbose $VERBOSE
+python3 -m llama_cpp.server --host 0.0.0.0 --port 8000 --model $MODEL_NAME --n_gpu_layers $N_GPU_LAYERS --n_ctx $N_CTX --n_batch $N_BATCH --main_gpu $MAIN_GPU --embedding $EMBEDDING --rope_freq_base $ROPE_FREQ_BASE --rope_freq_scale $ROPE_FREQ_SCALE --mul_mat_q $MUL_MAT_Q --f16_kv $F16_KV --logits_all $LOGITS_ALL --last_n_tokens_size $LAST_N_TOKENS_SIZE --numa $NUMA --chat_format '$CHAT_FORMAT' --verbose $VERBOSE
